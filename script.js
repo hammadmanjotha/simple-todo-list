2b0923d4921097c06f3dc1de0e945fa90cc17bd9
hammadmanjotha <54828352+hammadmanjotha@users.noreply.github.com>
2021-05-01 16:04:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let form = document.querySelector("#form");
+let input = document.querySelector(".input");
+let list = document.querySelector("#list");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let element = document.createElement("h2");
+  element.innerText = input.value;
+  element.classList.add("list-items");
+  list.appendChild(element);
+  input.value = "";
+
+  element.addEventListener("click", () => {
+    element.remove();
+  });
+});
